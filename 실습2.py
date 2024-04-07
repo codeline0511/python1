bc7f985a354143f2510523db735e770c7494c1a4
codeline0511 <89986502+codeline0511@users.noreply.github.com>
2024-04-07 19:12:52 +0900

    Delete 실습2.py

--- 실습2.py
+++
@@ -1,9 +0,0 @@
-def introduce(name, grade):
-    last_name = name[1:]
-    up_grade = grade + 1
-    return f'{last_name}은 내년에 {up_grade}학년입니다.'
-
-a = input('이름?')
-b = int(input('학년?'))
-result = introduce(a, b)
-print(result)
