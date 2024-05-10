dfd5b54e522900276e6e920d1bc40f1d2ba4c4e5
codeline0511 <89986502+codeline0511@users.noreply.github.com>
2024-05-10 22:02:13 +0900

    Delete 실습_1.py

--- 실습_1.py
+++
@@ -1,9 +0,0 @@
-def get_date():
-    y = int(input('연도? '))
-    m = int(input('월? '))
-    d = int(input('일? '))
-    return (y, m, d)
-
-print('당신의 생일을 입력하세요:')
-bday = get_date()
-print(f'당신의 만 60번째 생일은 {bday[0]+60}년 {bday[1]}월 {bday[2]}일입니다.')
